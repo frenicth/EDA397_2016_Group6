4df7439b9b912e9399fd4adabdaa6dabcb9d01d8
SALITIBI <salagvardit@msn.com>
2016-04-14 14:36:53 +0200

    Skeleton Android app.

---
+++ Pinder/app/src/androidTest/java/group6/eda397_2016/chalmers/se/pinder/ApplicationTest.java
@@ -0,0 +1,13 @@
+package group6.eda397_2016.chalmers.se.pinder;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

ccc761f2f44a02aefc1792f4d7e790045dcf9993
Fredrik Holmdahl <freholmd@student.chalmers.se>
2016-04-21 00:08:51 +0200

    added activities and fragments with corresponding views for Login and Register

---
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/LoginActivity.java
@@ -0,0 +1,56 @@
+package group6.eda397_2016.chalmers.se.pinder;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.app.Activity;
+import android.app.ActionBar;
+/**
+ * Created by fredrikholmdahl on 16-04-20.
+ */
+public class LoginActivity extends Activity {
+
+    public ActionBar actionBar;
+    public LoginFragment loginFragment;
+    public RegisterFragment registerFragment;
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        /*actionBar = getActionBar();
+        actionBar.hide();*/
+
+        loginFragment = new LoginFragment();
+        registerFragment = new RegisterFragment();
+
+        fragmentManager.beginTransaction().add(R.id.activity_login, loginFragment).commit();
+
+    }
+
+    public void register(View view){
+
+        fragmentTransaction= getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.activity_login, registerFragment);
+        fragmentTransaction.addToBackStack("register");
+        fragmentTransaction.commit();
+    }
+
+    public void login(View view){
+
+    }
+
+    public void cancelRegister(View view){
+        fragmentManager.popBackStack();
+    }
+
+}
---
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/RegisterFragment.java
@@ -0,0 +1,23 @@
+package group6.eda397_2016.chalmers.se.pinder;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by fredrikholmdahl on 16-04-20.
+ */
+public class RegisterFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for login
+        view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+}

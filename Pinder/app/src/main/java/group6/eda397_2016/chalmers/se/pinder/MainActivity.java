a2eedffb4f2d9caf022f7f2dc39503b78e83f41c
Fredrik Holmdahl <freholmd@student.chalmers.se>
2016-04-21 14:17:16 +0200

    made activites work with fragments and changed xml activites and fragments for Main

--- Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/MainActivity.java
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/MainActivity.java
@@ -1,5 +1,6 @@
 package group6.eda397_2016.chalmers.se.pinder;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,10 +10,18 @@ import group6.eda397_2016.chalmers.se.pinder.dao.Database;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getFragmentManager();
+    MainFragment mainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainFragment = new MainFragment();
+
+        fragmentManager.beginTransaction().add(R.id.activity_main, mainFragment).commit();
+
     }
     public void showTasks (View view ) {
         Database database = ((PinderApplication)getApplication()).getDatabase();
@@ -20,4 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void viewProfile(View view){
+        //view you own profile, get data from database
+    }
+    public void showProfiles(View view){
+        //view list of profiles, get data from database
+    }
 }

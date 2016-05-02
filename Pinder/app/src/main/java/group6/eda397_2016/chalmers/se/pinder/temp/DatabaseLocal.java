a383020bce2c9d3db7bb190300ccb3e4b6d0f612
Kyriakos Malamas <kyr88@hotmail.com>
2016-05-02 22:41:40 +0200

    Made changes based on the changes in Profile and Task classes. Now this is populated through the API calls, so the "ohn Doe" stuff are commented out

--- Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/temp/DatabaseLocal.java
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/temp/DatabaseLocal.java
@@ -1,5 +1,7 @@
 package group6.eda397_2016.chalmers.se.pinder.temp;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,17 +20,18 @@ public class DatabaseLocal implements Database{
     private static volatile DatabaseLocal instance;
     private volatile List<Profile> profiles;
     private volatile List<Task> tasks;
+    private  volatile Profile currentUser;
 
     private DatabaseLocal(){
         profiles = new ArrayList<>();
         tasks = new ArrayList<>();
-        List<Skill> skills1 = new ArrayList<>();
-        skills1.add(Skill.CPlusPlus);
-        skills1.add(Skill.Java);
-        profiles.add(new Profile(0,"John Doe",skills1));
-        profiles.add(new Profile(1,"Jane Doe",new ArrayList<Skill>()));
-        tasks.add(new Task(0,"Task 1","The first task",5));
-        tasks.add(new Task(1,"Task 2","The second task",3));
+        //List<Skill> skills = new ArrayList<>();
+        //skills.add(Skill.CPlusPlus);
+        //skills.add(Skill.Java);
+        //profiles.add(new Profile(0,"John Doe",skills1));
+        //profiles.add(new Profile(1,"Jane Doe",new ArrayList<Skill>()));
+        //tasks.add(new Task(0,"Task 1","The first task",5));
+        //tasks.add(new Task(1,"Task 2","The second task",3));
     }
 
     public synchronized static DatabaseLocal getInstance(){
@@ -39,23 +42,29 @@ public class DatabaseLocal implements Database{
     }
 
     @Override
-    public List<Task> getAllTasks() {
+    public List<Task> getAllTasks()
+    {
         return tasks;
     }
 
     @Override
-    public List<Profile> getAllProfiles() {
+    public List<Profile> getAllProfiles()
+    {
         return profiles;
     }
 
     @Override
-    public void createProfile(Profile profile) {
+    public void createProfile(Profile profile)
+    {
         profiles.add(profile);
+        Log.d("DatabaseLocal class", "added profile with id: "+profile.getId());
     }
 
     @Override
-    public void createTask(Task task) {
+    public void createTask(Task task)
+    {
         tasks.add(task);
+        Log.d("DatabaseLocal class", "added task with id: "+task.getId());
     }
 
     @Override
@@ -64,12 +73,26 @@ public class DatabaseLocal implements Database{
     }
 
     @Override
-    public Task getTaskById(int id) {
+    public Task getTaskById(int id)
+    {
         return null;
     }
 
     @Override
-    public Profile getProfileById(int id) {
+    public Profile getProfileById(int id)
+    {
         return null;
     }
+
+    public void setCurrentUser(Profile profile)
+    {
+        this.currentUser = profile;
+        Log.d("DatabaseLocal class", "set current User: "+profile.getName());
+    }
+
+    public Profile getCurrentUser()
+    {
+        return this.currentUser;
+    }
+
 }

db4ae29dfe1230084d8ff9e29f0efa31b9220fa5
Kyriakos Malamas <kyr88@hotmail.com>
2016-05-02 22:39:19 +0200

    Restructured the class to match the data we need and fetch from the API and updated all relevant code in other classes/methods

--- Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/model/Profile.java
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/model/Profile.java
@@ -1,42 +1,94 @@
 package group6.eda397_2016.chalmers.se.pinder.model;
 
+import android.util.Log;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Profile {
-    private int id;
+    private String id;
     private String name;
-    private List<Skill> skills;
+    private List<String> skills;
+    private String bio;
 
-    public Profile(int id, String name, List<Skill> skills) {
+    public Profile () {}
+
+    public Profile(String id, String name) {
         this.id = id;
         this.name = name;
-        this.skills = skills;
+        this.skills = new ArrayList<>();
     }
 
-    public Profile() {
+    public Profile(String id, String name, String bio) {
+        this.id = id;
+        this.name = name;
+        this.skills = new ArrayList<>();
+        parseBio(bio);
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public List<Skill> getSkills() {
+    public List<String> getSkills()
+    {
         return skills;
     }
 
-    public void setSkills(List<Skill> skills) {
-        this.skills = skills;
+
+    public void addSkill(String skill)
+    {
+        if (skill.contains(","))
+        {
+            String [] temp = skill.split(",");
+            for (int i=0;i<temp.length;i++)
+            {
+                if (temp[i]!=null)
+                skills.add(temp[i].trim());
+            }
+        }
+        else
+        {
+            skills.add(skill);
+        }
     }
 
+    private void parseBio (String bio)
+    {
+        if (!bio.isEmpty())
+        {
+            if (bio.contains("Skills")) {
+                try {
+                    String skills = bio.substring((bio.lastIndexOf(":")+1));
+                    this.addSkill(skills);
+                    this.bio = bio.substring(0, bio.indexOf("Skills") - 1);
+                }
+                catch (Exception e)
+                { Log.e("Profile Creation", "Not expected format for bio in Profile: " + this.name); }
+                finally
+                {this.bio = bio;}
+            }
+            else
+            {
+                this.bio = bio;
+            }
+
+        }
+    }
+
+    public String getId()
+    {return this.id;}
     @Override
     public String toString() {
         String ret = name + " Skills: ";
-        for (Skill skill : skills) {
-            ret += skill.name() + " ";
+        for (String skill : skills) {
+            ret += skill + " ";
         }
         return ret;
     }

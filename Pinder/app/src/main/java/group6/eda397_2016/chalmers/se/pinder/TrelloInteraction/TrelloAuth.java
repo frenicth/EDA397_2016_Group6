2e64e56f3c2c37c915b72d55a7c7ff9bfb41b880
Kyriakos Malamas <kyr88@hotmail.com>
2016-05-09 10:48:16 +0200

    Changed scope (permissions) for the authKey to read,write, account so we can do posts and change things in profile like bio (Skills)

--- Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/TrelloInteraction/TrelloAuth.java
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/TrelloInteraction/TrelloAuth.java
@@ -13,7 +13,8 @@ public class TrelloAuth {
     public final static String expiration = "never";
     public final static String callbackMethod = "fragment";
     public final static String returnUrl = "ase://oauthresponse";
-    public final static String trelloAuthorizeUrl = "https://trello.com/1/authorize?expiration=" + expiration + "&name=" + appName + "&key=" + appKey + "&callback_method=" + callbackMethod + "&return_url=" + returnUrl;
+    public final static String scope = "read,write,account";
+    public final static String trelloAuthorizeUrl = "https://trello.com/1/authorize?expiration=" + expiration + "&name=" + appName + "&key=" + appKey + "&callback_method=" + callbackMethod + "&return_url=" + returnUrl+"&scope="+ scope;
 
 
 }

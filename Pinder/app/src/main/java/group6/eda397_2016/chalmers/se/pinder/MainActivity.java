53938ecdc9a987e4c05a33b219fd46ba3c0165f4
Fredrik Holmdahl <freholmd@student.chalmers.se>
2016-05-12 16:33:59 +0200

    fixed error and removed/cleaned code/classes

--- Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/MainActivity.java
+++ Pinder/app/src/main/java/group6/eda397_2016/chalmers/se/pinder/MainActivity.java
@@ -7,10 +7,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -32,11 +39,14 @@ public class MainActivity extends AppCompatActivity {
     TasksFragment tasksFragment;
     UserProfileFragment userProfileFragment;
     public android.support.v7.app.ActionBar actionBar;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Database database = ((PinderApplication)getApplication()).getDatabase();
         super.onCreate(savedInstanceState);
+        /* OLD since tabbed navigation was implemented
         setContentView(R.layout.activity_main);
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(false);
@@ -44,14 +54,96 @@ public class MainActivity extends AppCompatActivity {
         profilesFragment = new ProfilesFragment();
         tasksFragment = new TasksFragment();
         userProfileFragment = new UserProfileFragment();
+        */
         database.clearDB();
         TrelloAPIConsumer.fetchUserProfile("me", this.getApplicationContext());
         TrelloAPIConsumer.fetchTeamMembers(this.getApplicationContext());
         TrelloAPIConsumer.fetchBackLogTasks(this.getApplicationContext());
+        /* OLD since tabbed navigation was implemented
         fragmentManager.beginTransaction().add(R.id.activity_main, mainFragment).commit();
+        */
+        setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Do we need to add tasks inside the app?", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            SharedPreferences sharedPreferences = this.getSharedPreferences("authorizeprefs", Context.MODE_PRIVATE);
+            sharedPreferences.edit().putString("authtoken", "empty").apply();
+            Toast.makeText(MainActivity.this, "You are now logged out", Toast.LENGTH_SHORT).show();
+            Intent log = new Intent(this, LoginActivity.class);
+            startActivity(log);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            System.out.println(position);
+            switch (position) {
+                case 0:
+                    return UserProfileFragment.newInstance();
+                case 1:
+                    return TasksFragment.newInstance();
+                case 2:
+                    return ProfilesFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
     /*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -115,5 +207,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     */
-
+    }
 }
